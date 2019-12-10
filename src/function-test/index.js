2ef558d778860a8d9f68f26075edf34c139963c9
Manuel <enrique.garcia@ironbit.com.mx>
2019-12-09 18:18:22 -0600

    feat: template testing template for testing. Add unit testing for weather arrays

---
+++ src/function-test/index.js
@@ -0,0 +1,48 @@
+function minUmbrellas(weather) {
+    const daysWeatherBoolean = [];
+    let totalUmbrellas = 0;
+    let umbrellasHome = 0;
+    let umbrellasOffice = 0;
+    let home = true;
+    //Converting rainy and thunderstorm to true and false
+    weather.map(element => {
+      if (element === "rainy" || element === "thunderstorms"){
+        daysWeatherBoolean.push(1);
+      }else{
+        daysWeatherBoolean.push(0)
+      }
+    });
+    
+    for (let i in daysWeatherBoolean){
+      if (daysWeatherBoolean[i] === 1  && home === true){
+        if(umbrellasHome === 0){
+          totalUmbrellas +=1;
+          umbrellasOffice +=1;
+          home = false;
+        }else if(umbrellasHome > 0){
+          umbrellasHome -= 1;
+          umbrellasOffice +=1;
+          home = false;
+        }
+      } else if (daysWeatherBoolean[i] === 1 && home === false){
+        if(umbrellasOffice === 0){
+          totalUmbrellas +=1;
+          umbrellasHome +=1
+          home = true;
+        }else if(umbrellasOffice > 0){
+          umbrellasOffice -= 1;
+          umbrellasHome +=1;
+          home = true;
+        } 
+      } else if (daysWeatherBoolean[i] === 0  && home === true){
+        home = false;
+      } else{
+        home = true;
+      }
+    }
+   
+    return totalUmbrellas;
+}
+
+
+module.exports = minUmbrellas;
---
+++ test/function-test/index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const assert = require('chai').assert;
+const minUmbrellas = require('../../src/function-test');
+
+suite('All cases', () => {
+    test('Success case', () => {
+        assert.equal(minUmbrellas(["cloudy"]), 0, "wrong answer for one value in weather array");
+    });
+    test('Success case', () => {
+        assert.equal(minUmbrellas(["rainy", "rainy", "rainy", "rainy"]), 1, "wrong answer for always rainy.");
+    });
+    test('Success case', () => {
+        assert.equal(minUmbrellas(["overcast", "rainy", "clear", "thunderstorms"]), 2, "wrong answer for 2 dry mornings and 2 rainy afternoons.");
+    });
+    test('Success case', () => {
+        assert.equal(minUmbrellas(["overcast", "rainy", "clear", "thunderstorms"]), 2, "wrong answer for 2 dry mornings and 2 rainy afternoons.");
+    });
+    test('Success case', () => {
+        assert.equal(minUmbrellas(["rainy", "rainy", "clear", "rainy", "clear", "rainy", "clear", "rainy"]), 4, "wrong answer for 2 dry mornings and 2 rainy afternoons.");
+    });
+    test('Success case', () => {
+        assert.equal(minUmbrellas(["overcast", "rainy", "clear", "overcast", "rainy", "clear", "thunderstorms", "rainy", "clear"]), 2, "wrong answer for 2 dry mornings and 2 rainy afternoons.");
+    });
+});
